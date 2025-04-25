246ec80c7232df91215fd741fbf4c2126de0fe2c
Levi Kaugu <levikaugu@gmail.com>
2025-04-26 01:34:32 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
-import favicon from "@/app/favicon.svg"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
