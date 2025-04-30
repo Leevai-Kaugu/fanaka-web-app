f890b14373c809a986d6dcb3805f19d87b5c921a
Levi Kaugu <levikaugu@gmail.com>
2025-04-30 12:45:13 +0300

    fixed footer nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,12 +33,21 @@ export default function RootLayout({
           <div className="bg-foreground text-background flex md:flex-row flex-col-reverse w-full justify-between items-center p-[20px] mt-[35vh]">
             <p className="w-full md:w-1/2 text-center md:text-start mt-10 md:mt-[0px]">Copyright © {currentYear} Fanaka Tech. All rights reserved.</p>
             <div>
-              <ul className="grid grid-cols-2 md:grid-cols-6 text-center md:text-start ">
+              <ul className="hidden md:grid grid-cols-6 text-center text-start">
                 <li className="m-[10px] hover:text-fp transition-all duration-300 ease-in"><Link href="/">Home</Link></li>
                 <li className="m-[10px] hover:text-fp transition-all duration-300 ease-in"><Link href="partners">Partners</Link></li>
                 <li className="m-[10px] hover:text-fp transition-all duration-300 ease-in"><Link href="about">About</Link></li>
                 <li className="m-[10px] hover:text-fp transition-all duration-300 ease-in"><Link href="career">Careers</Link></li>
                 <li className="m-[10px] hover:text-fp transition-all duration-300 ease-in"><Link href="contact">Contact</Link></li>
+              </ul>
+              <ul className="flex items-center flex-col md:hidden">
+                <div className="grid grid-cols-2 md:grid-cols-6 text-center md:text-start ">
+                  <li className="m-[10px] hover:text-fp transition-all duration-300 ease-in"><Link href="/">Home</Link></li>
+                  <li className="m-[10px] hover:text-fp transition-all duration-300 ease-in"><Link href="partners">Partners</Link></li>
+                  <li className="m-[10px] hover:text-fp transition-all duration-300 ease-in"><Link href="about">About</Link></li>
+                  <li className="m-[10px] hover:text-fp transition-all duration-300 ease-in"><Link href="career">Careers</Link></li>
+                </div>
+                  <li className="m-[10px] hover:text-fp transition-all duration-300 ease-in"><Link href="contact">Contact</Link></li>
               </ul>
             </div>
         </div>
